e6dcc6a6a9da653603965c54e5045a7dbdbfafb4
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-08-28 15:40:58 +0300

    Income/outcome tabs

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { ThemeProvider } from './components/theme-provider';
-import { Home } from './pages';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { ThemeProvider } from "./components/theme-provider";
+import { Home } from "./pages";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route path="/" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,52 @@
-import { Card, CardContent } from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Plus } from "lucide-react";
+
+type TransactionType = "IN" | "OUT";
 
 export default function Home() {
+  const [tab, setTab] = useState<TransactionType>("OUT");
+
   return (
-    <Card className='w-9/12 mx-auto'>
-      <CardContent>
-        text
-      </CardContent>
-    </Card>
+    <>
+      <div className="sticky my-5 w-screen">
+        <Tabs defaultValue="OUT" onValueChange={setTab}>
+          <TabsList className="grid w-4/5 grid-cols-2 mx-auto">
+            <TabsTrigger value="OUT">Expenses</TabsTrigger>
+            <TabsTrigger value="IN">Income</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+      <Select defaultValue="month">
+        <SelectTrigger className="w-28 mx-auto">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="day">Day</SelectItem>
+          <SelectItem value="week">Week</SelectItem>
+          <SelectItem value="month">Month</SelectItem>
+          <SelectItem value="year">Year</SelectItem>
+          <SelectSeparator></SelectSeparator>
+          <SelectItem value="custom">Custom</SelectItem>
+        </SelectContent>
+      </Select>
+      <div className="fixed bottom-6 w-screen">
+        <Button size="icon" className="flex mx-auto rounded-full">
+          <Plus />
+        </Button>
+      </div>
+    </>
   );
 }
