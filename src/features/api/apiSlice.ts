50c720bc203c71fecbef8c8beba12813bf8b00a2
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-09-07 10:02:26 +0300

    Automatically refresh token

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,26 +1,68 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  createApi,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react';
 import Cookies from 'js-cookie';
+import { Mutex } from 'async-mutex';
+import { REHYDRATE } from 'redux-persist';
 import {
   JwtToken,
   LoginRequest,
   RegistrationRequest,
   RegistrationResponse, 
 } from './types';
-import { REHYDRATE } from 'redux-persist';
 import { RootState } from '../../store';
+import { setToken } from './auth';
+
+const reauthMutex = new Mutex();
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'http://localhost:80/',
+  prepareHeaders: (headers, { getState }) => {
+    const csrfToken = Cookies.get('csrftoken');
+    csrfToken && headers.set('x-csrftoken', csrfToken);
+    const token = (getState() as RootState).token;
+    token && headers.set('authorization', `Bearer ${token}`);
+    return headers;
+  },
+  credentials: 'include',
+});
+
+const baseQueryWithReauth: BaseQueryFn<
+string | FetchArgs, unknown, FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  await reauthMutex.waitForUnlock();
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    if (!reauthMutex.isLocked()) {
+      const release = await reauthMutex.acquire();
+      try {
+        const refreshResult = await baseQuery(
+          {
+            url:'/accounts/auth/jwt/refresh/',
+            method: 'POST',
+          }, api, extraOptions
+        );
+        if (refreshResult.data) {
+          const {
+            access: accessToken,
+          } = refreshResult.data as { access: string };
+          api.dispatch(setToken(accessToken));
+          result = await baseQuery(args, api, extraOptions);
+        }
+      } finally {
+        release();
+      }
+    }
+  }
+  return result;
+};
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:80/',
-    prepareHeaders: (headers, { getState }) => {
-      const csrfToken = Cookies.get('csrftoken');
-      csrfToken && headers.set('x-csrftoken', csrfToken);
-      const token = (getState() as RootState).token;
-      token && headers.set('authorization', `Bearer ${token}`);
-      return headers;
-    },
-    credentials: 'include',
-  }),
+  baseQuery: baseQueryWithReauth,
   extractRehydrationInfo(action, { reducerPath }) {
     if (action.type === REHYDRATE) {
       return action.payload[reducerPath];
