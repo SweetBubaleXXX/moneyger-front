cbf0aea30004cf160c491dfd0f24ff5edb27038f
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-09-09 08:32:00 +0300

    Use ROUTER_PATHS in router

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
+import { ROUTER_PATHS } from './constants';
 
 export default () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path={ROUTER_PATHS.home} element={<Home />}></Route>
+        <Route path={ROUTER_PATHS.login} element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
