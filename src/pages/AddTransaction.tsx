aebf27fb2dee234c5b4b7e5bb235f755e390ea66
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-09-14 13:39:23 +0300

    Transaction adding page back button

--- src/pages/AddTransaction.tsx
+++ src/pages/AddTransaction.tsx
@@ -1,21 +1,51 @@
 import React from 'react';
-import { Box } from '@mui/joy';
+import { useNavigate } from 'react-router-dom';
+import {
+  Box, 
+  Grid,
+  IconButton,
+  Typography,
+} from '@mui/joy';
 import {
   TransactionCreateForm, 
 } from '../components/transactions/TransactionCreateForm';
+import { ChevronLeft } from 'lucide-react';
 
 export const AddTransaction = () => {
+  const navigate = useNavigate();
+  
   return (
-    <Box 
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      minHeight="100vh"
-      minWidth={230}
-      maxWidth={300}
-      marginX="auto"
-    >
-      <TransactionCreateForm/>
-    </Box>
+    <>
+      <Box 
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-around"
+        minHeight="100vh"
+      >
+        <Box
+          display="flex"
+          maxWidth="md"
+          p={2}
+          mx="auto"
+        >
+          <IconButton onClick={() => navigate(-1)}>
+            <ChevronLeft />
+          </IconButton>
+          <Typography
+            level="h3"
+            textAlign="center"
+          >
+            Add Transaction
+          </Typography>
+        </Box>
+        <Box
+          minWidth={230}
+          maxWidth={300}
+          marginX="auto"
+        >
+          <TransactionCreateForm />
+        </Box>
+      </Box>
+    </>
   );
 };
