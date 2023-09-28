f4b8b728ba48ce4ab35e8befc3a0241774779279
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-09-28 11:24:45 +0300

    Add default currency setting

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,6 @@
 import {
   List,
+  ListDivider,
   ListItem,
   ListItemContent,
   Option,
@@ -9,13 +10,45 @@ import {
 import React from 'react';
 
 import { NavigationBar } from '../components/toolbars/NavigationBar';
+import { CURRENCY_CODES } from '../constants';
+import { useGetAccountQuery } from '../features/api/apiSlice';
 
 export const Settings = () => {
   const { mode, setMode } = useColorScheme();
 
+  const account = useGetAccountQuery();
+
   return (
     <>
-      <List variant="outlined">
+      <List
+        variant="soft"
+        sx={{
+          m: 2,
+          borderRadius: 'md',
+        }}
+      >
+        <ListItem endAction={
+          <Select
+            disabled={account.isFetching}
+            value={account.data?.defaultCurrency}
+          >
+            {
+              CURRENCY_CODES.map(curCode =>
+                <Option
+                  value={curCode}
+                  key={curCode}
+                >
+                  {curCode}
+                </Option>
+              )
+            }
+          </Select>
+        }>
+          <ListItemContent>
+            Default Currency
+          </ListItemContent>
+        </ListItem>
+        <ListDivider />
         <ListItem endAction={
           <Select
             value={mode}
